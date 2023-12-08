14ab541fb9f2b1004340e80dd0720a17d27f0cf3
Shivam1272 <96972819+Shivam1272@users.noreply.github.com>
2023-12-08 12:05:21 +0000

    math and num

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,42 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100.1234)
+/*
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(balance.toPrecision(6)); // return string value
+*/
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ********************** MATHS **********************//
+
+// console.log(Math); // run this on browser to get more info
+
+/*
+console.log(Math.abs(-1));
+console.log(Math.round(4.7));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.3));
+*/
+
+const arr = [10,2, 34, 543,3]
+
+/*
+console.log(Math.max(1,2,4 ,5)) // it dosen't take array parameter
+console.log(Math.min(12,1,31,34,4,45))
+*/
+/*
+console.log(Math.random()); // always give value between 0, 1
+console.log((Math.random()*10) + 1);
+*/
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min);
