6a9ad77c6d38f0cffc2994fb2adac9082b2cdb98
Shivam1272 <96972819+Shivam1272@users.noreply.github.com>
2023-12-08 06:06:49 +0000

    datatypes in JS

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all JS code as newer version
+
+// alert("Hello") // it will throw error because we are using nodejs, not browser
+
+let name = "Shivam"
+let age = 21
+let isLearning = true
+
+/*
+Primitive Data type:
+    number
+    bigInt
+    string => ""/''
+    boolean => true/false
+    null => standalone value
+    undefined
+    symbol => unique
+*/
+
+console.log(typeof undefined);
+console.log(typeof null);
