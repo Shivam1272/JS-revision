7eb7d025043df01367d099ec7a4a2bb151b263b2
Shivam1272 <96972819+Shivam1272@users.noreply.github.com>
2023-12-09 06:36:03 +0000

    while and do while

---
+++ 05_iterations/02_while_doWhile_loops.js
@@ -0,0 +1,19 @@
+// while
+
+
+let i = 1
+
+while (i <= 10 ) {
+    // console.log(`Value of i is ${i}`);
+    i = i + 2
+}
+
+
+// do while
+
+let score = 11 
+
+do {
+    // console.log('Score', score);
+    score++;
+} while (score <= 10);
